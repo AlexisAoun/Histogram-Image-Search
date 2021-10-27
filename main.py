af1f60743aad3c6d5403f5ce56b2e2a9d20416b6
Alexis Aoun <alexis.aoun@hotmail.com>
2021-10-27 12:06:36 +0200

    feat: introduction to debug log

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt 
 
-queryImg = cv.imread('./data/small/queries/152029243_b3582c36fa.jpg', cv.IMREAD_COLOR)
+debug = True
+
+if debug :
+    print("[DEBUG] Chargement de l'image")
+
+try : 
+    queryImg = cv.imread('./data/small/queries/152029243_b3582c36fa.jpg', cv.IMREAD_COLOR)
+except ValueError:
+    print("[DEBUG] Echec du chargement de l'image")
+
+if debug and queryImg is None :
+    print("[DEBUG] Echec du chargement de l'image")
+    exit()
+elif debug and queryImg.all() != None :
+    print("[DEBUG] Image chargée avec succés")
 
 rHistoQuery = cv.calcHist([queryImg], [0], None, [256], [0, 256])
 gHistoQuery = cv.calcHist([queryImg], [1], None, [256], [0, 256])
