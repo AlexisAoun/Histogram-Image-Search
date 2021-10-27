688ab5c680ff8a23d38a6b1c418f5780f0675d5a
Alexis Aoun <alexis.aoun@hotmail.com>
2021-10-27 13:33:49 +0200

    Refactor: create computeHistoVector func

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ debug = True
 if debug :
     print("[DEBUG] Chargement de l'image requete...")
 
+def computeHistoVector (inputImage) :
+
+    rHisto = cv.calcHist([inputImage], [0], None, [256], [0, 256])
+    gHisto = cv.calcHist([inputImage], [1], None, [256], [0, 256])
+    bHisto = cv.calcHist([inputImage], [2], None, [256], [0, 256])
+
+    rHisto = rHisto.flatten()
+    gHisto = gHisto.flatten()
+    bHisto = bHisto.flatten()
+
+    rHisto = cv.normalize(rHisto, rHisto)
+    gHisto = cv.normalize(gHisto, gHisto)
+    bHisto = cv.normalize(bHisto, bHisto)
+
+    histo = np.concatenate((rHisto, gHisto, bHisto))
+
+    return histo
+
 try : 
     queryImg = cv.imread('./data/small/queries/152029243_b3582c36fa.jpg', cv.IMREAD_COLOR)
 except ValueError:
@@ -21,22 +39,16 @@ if debug and queryImg is None :
 elif debug and queryImg.all() != None :
     print("[DEBUG] Image requete chargée avec succés")
 
+
 if debug :
     print("[DEBUG] Calcule du vecteur carateristique de la requete...")
-rHistoQuery = cv.calcHist([queryImg], [0], None, [256], [0, 256])
-gHistoQuery = cv.calcHist([queryImg], [1], None, [256], [0, 256])
-bHistoQuery = cv.calcHist([queryImg], [2], None, [256], [0, 256])
+    
+    histoQuery = computeHistoVector(queryImg)
 
-rHistoQuery = rHistoQuery.flatten()
-gHistoQuery = gHistoQuery.flatten()
-bHistoQuery = bHistoQuery.flatten()
-
-rHistoQuery = cv.normalize(rHistoQuery, rHistoQuery)
-gHistoQuery = cv.normalize(gHistoQuery, gHistoQuery)
-bHistoQuery = cv.normalize(bHistoQuery, bHistoQuery)
-
-histoQuery = np.concatenate((rHistoQuery, gHistoQuery, bHistoQuery))
-
-print(histoQuery.shape)
-if debug and histoQuery is not None and histoQuery.shape == (768,):
-    print("[DEBUG] Vecteur carateristique de la requete calculé (shape OK)")
+if debug : 
+    if histoQuery is not None and histoQuery.shape == (768,) :
+        print("[DEBUG] Vecteur carateristique de la requete calculé (shape OK)")
+    else : 
+        print("[DEBUG] Echec du calcul du vecteur caractéristique de la requete")
+        print("[DEBUG] Sorti du programme...")
+        exit()
