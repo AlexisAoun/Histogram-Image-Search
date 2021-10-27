56b87f56fcb9a8eb19e0c74f1326de24c5e07e92
Alexis Aoun <alexis.aoun@hotmail.com>
2021-10-27 13:11:26 +0200

    feat: more debug

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ from matplotlib import pyplot as plt
 debug = True
 
 if debug :
-    print("[DEBUG] Chargement de l'image")
+    print("[DEBUG] Chargement de l'image requete...")
 
 try : 
     queryImg = cv.imread('./data/small/queries/152029243_b3582c36fa.jpg', cv.IMREAD_COLOR)
 except ValueError:
-    print("[DEBUG] Echec du chargement de l'image")
+    print("[DEBUG] Echec du chargement de l'image requete")
+    print("[DEBUG] Sorti du programme...")
+    exit()
 
 if debug and queryImg is None :
-    print("[DEBUG] Echec du chargement de l'image")
+    print("[DEBUG] Echec du chargement de l'image requete")
+    print("[DEBUG] Sorti du programme...")
     exit()
 elif debug and queryImg.all() != None :
-    print("[DEBUG] Image chargée avec succés")
+    print("[DEBUG] Image requete chargée avec succés")
 
+if debug :
+    print("[DEBUG] Calcule du vecteur carateristique de la requete...")
 rHistoQuery = cv.calcHist([queryImg], [0], None, [256], [0, 256])
 gHistoQuery = cv.calcHist([queryImg], [1], None, [256], [0, 256])
 bHistoQuery = cv.calcHist([queryImg], [2], None, [256], [0, 256])
@@ -31,3 +36,7 @@ gHistoQuery = cv.normalize(gHistoQuery, gHistoQuery)
 bHistoQuery = cv.normalize(bHistoQuery, bHistoQuery)
 
 histoQuery = np.concatenate((rHistoQuery, gHistoQuery, bHistoQuery))
+
+print(histoQuery.shape)
+if debug and histoQuery is not None and histoQuery.shape == (768,):
+    print("[DEBUG] Vecteur carateristique de la requete calculé (shape OK)")
