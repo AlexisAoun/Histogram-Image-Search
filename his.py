58b242f0f1f61b7a1eefc5d3ffe3b71cbefe6716
Alexis Aoun <alexis.aoun@hotmail.com>
2021-11-03 23:48:05 +0100

    feat: displaying an image

--- his.py
+++ his.py
@@ -1,9 +1,13 @@
 from tkinter import *
 from tkinter import ttk
+from PIL import ImageTk, Image
 
 root = Tk()
 root.title("Histogram Image Search")
 
-mainframe = ttk.Frame(root)
+mainframe = ttk.Frame(root).grid(column=0, row=0)
+image = ImageTk.PhotoImage(Image.open("data/small/queries/96978713_775d66a18d.jpg"))
+imageLabel = ttk.Label(mainframe, image = image).grid()
+
 
 root.mainloop()
