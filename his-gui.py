134d8b7e379d584625d13bf254250ffe7d172e1e
Alexis Aoun <alexis.aoun@hotmail.com>
2021-11-08 16:52:36 +0100

    feat: image resizing based on win width

--- his-gui.py
+++ his-gui.py
@@ -4,29 +4,52 @@ from PIL import ImageTk, Image
 
 root = Tk()
 root.title("Histogram Image Search")
-root.geometry("1200x1200")
+root.geometry("800x800")
 
 
-paths  = ["data/small/queries/96978713_775d66a18d.jpg", "data/small/queries/3178371973_60c6b8f110.jpg",
- "data/small/queries/3074617663_2f2634081d.jpg", "data/small/queries/1924234308_c9ddcf206d.jpg"]
+paths = [
+    "data/small/queries/96978713_775d66a18d.jpg",
+    "data/small/queries/3178371973_60c6b8f110.jpg",
+    "data/small/queries/3074617663_2f2634081d.jpg",
+    "data/small/queries/1924234308_c9ddcf206d.jpg",
+]
 
 mainframe = ttk.Frame(root)
+resultsFrame = ttk.Frame(mainframe)
 mainframe.grid(column=0, row=0)
+resultsFrame.grid(column=0, row=1)
 
-root.update()
-
-winWidth = root.winfo_width()
-winHeight = root.winfo_height()
-
-print(str(winWidth))
 images = []
 imageLabels = []
+imageRatios = []
 
-for path in paths: 
+for path in paths:
     image = ImageTk.PhotoImage(Image.open(path))
-    imageLabel = ttk.Label(mainframe, image=image, text=path)
+    imageLabel = ttk.Label(resultsFrame, image=image)
+    imageRatio = image.width() / image.height()
+
     images.append(image)
     imageLabels.append(imageLabel)
+    imageRatios.append(imageRatio)
+
+def resizeImage(width, height, index):
+    data = Image.open(paths[index])
+    dataResized = data.resize((width, height), Image.ANTIALIAS)
+    imageResized = ImageTk.PhotoImage(dataResized)
+    imageLabels[index].configure(image=imageResized)
+    imageLabels[index].image = imageResized
+
+
+def resizeAllImages():
+    root.update()
+    winWidth = root.winfo_width()
+    for i in range(len(paths)):
+        resizeImage(
+            width=int(winWidth / 4), height=int((winWidth / 4) / imageRatios[i]), index=i
+        )
+    resultsFrame.configure(padding=((1/4*winWidth)/2,3,3,0))
+
+resizeAllImages()
 
 imageLabels[0].grid(column=2, row=1)
 imageLabels[1].grid(column=1, row=2)
@@ -36,17 +59,11 @@ imageLabels[3].grid(column=3, row=2)
 # TODO : Make image size responsive (by taking width and height of window and update size
 # of images)
 
-def resizeImage(width, height, index):
-    data = Image.open(paths[index])
-    dataResized = data.resize((width, height), Image.ANTIALIAS)
-    imageResized = ImageTk.PhotoImage(dataResized)
-    imageLabels[index].configure(image=imageResized)
-    imageLabels[index].image = imageResized
-
 def resizeImageCallback():
-    resizeImage(200,200,3)
+    resizeImage(200, 200, 3)
 
-button = ttk.Button(mainframe, text='resize', command = resizeImageCallback).grid(column=2, row=3)
+button = ttk.Button(mainframe, text="resize", command=resizeImageCallback).grid(
+    column=2, row=3
+)
 
 root.mainloop()
-
