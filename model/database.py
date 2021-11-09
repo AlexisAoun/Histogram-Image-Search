529091866aa88a570567d14baaa7afc70d498381
Alexis Aoun <alexis.aoun@hotmail.com>
2021-11-09 19:45:52 +0100

    feat: database class

--- model/database.py
+++ model/database.py
@@ -1,9 +1,58 @@
+import os
+
 from utils import *
 
 class Database:
-    __paths = [] 
-    __vectors = []
+    __path = "" 
+    __imagePaths = []
+    __vectors = None
     __name = ""
     __savePath = ""
+    __size = 0
+
+
+    def __init__(self, name, path, savePath="."):
+        self.__name = name
+        self.__path = path 
+        self.__savePath = savePath + "/" + self.__name + ".npy" 
+
+        #generate array containing the paths of all the entries on our database
+        tmp = self.generateImageNames(self.__path)
+        self.__imagePaths = tmp[0]
+        self.__size = tmp[1]
+
+        #check wether the database is already saved
+        if self.__savePath.is_file():
+            self.__vectors = np.load(str(self.__savePath))
+        else:
+            self.__vectors = self.generateVectors(self.__imagePaths, self.__size)
+            
+
+    # args: path of the database
+    # return: array with the paths of all elements in the databse (png and/or jpg)
+    def generateImageNames(self, path):
+        output = []
+        size = 0
+        for entry in os.scandir(path):
+            if (entry.path.endswith(".jpg") or entry.path.endswith(".png")) and entry.is_file():
+                output.append(str(entry.path))
+                size += 1
+
+        return output, size
+
+
+    # args: paths: array of the paths of all the elements
+    #        size: size of the database
+    # return: array of the vectors of all the images in the database
+    def generateVectors(self, paths, size):
+        output = np.ndarray(shape=(size, 768))
+        i = 0
+
+        for path in paths:
+            output[i] = computeHistoVector(getImage((path)))
+            i += 1
+        
+        return output
+
 
 
